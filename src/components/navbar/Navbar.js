751b5ee0c98109118a8a17a8e5fc28994022a30e
nodirhajiev <nodir.hajiev@gmail.com>
2021-12-15 20:21:03 +0500

    master

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React from 'react'
 import logo from '../../assets/images/LOGO.svg'
 
-function Navbar({daySum,allSum}) {
-    return (
-        <div className="navbar">
-            <div className="container">
-                <div className="navbar__row">
-                    <div className="navbar__left">
-                        <p className="navbar__left-text">бугунги сумма:</p>
-                        <h3 className="navbar__left-price">{new Intl.NumberFormat('decimal').format(daySum)} сум</h3>
-                        <p className="navbar__left-day">24 Май 2021</p>
-                    </div>
+function Navbar({ daySum, allSum }) {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar__row">
+          <div className="navbar__left">
+            <p className="navbar__left-text">бугунги сумма:</p>
+            <h3 className="navbar__left-price">
+              {new Intl.NumberFormat('decimal').format(daySum)} сум
+            </h3>
+            <p className="navbar__left-day">24 Май 2021</p>
+          </div>
 
-                    <div className="navbar__logo">
-                        <div className="navbar__logo-wrap">
-                            <img src={logo} alt="logo_img"/>
-                        </div>
-                    </div>
-
-                    <div className="navbar__right">
-                        <p className="navbar__right-text">
-                            умумий келган пул:
-                        </p>
-                        <h3 className="navbar__right-title">{new Intl.NumberFormat('decimal').format(allSum)} сум</h3>
-                    </div>
-                </div>
+          <div className="navbar__logo">
+            <div className="navbar__logo-wrap">
+              <img src={logo} alt="logo_img" />
             </div>
+          </div>
+
+          <div className="navbar__right">
+            <p className="navbar__right-text">умумий келган пул:</p>
+            <h3 className="navbar__right-title">
+              {new Intl.NumberFormat('decimal').format(allSum)} сум
+            </h3>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  )
 }
 
-export default Navbar;
+export default Navbar
